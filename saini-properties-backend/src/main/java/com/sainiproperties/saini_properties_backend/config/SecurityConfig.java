49ed940bb9a101eff1488fea60bf0d127cce297f
Vansh <saini40vansh@gmail.com>
2026-07-24 01:17:26 +0530

    updated the SecurityConfig.java

--- saini-properties-backend/src/main/java/com/sainiproperties/saini_properties_backend/config/SecurityConfig.java
+++ saini-properties-backend/src/main/java/com/sainiproperties/saini_properties_backend/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/error",
                                 "/auth/**",
                                 "/api/contact-query",
                                 "/api/inquiries",
